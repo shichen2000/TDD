916a14e529a1028aad677e09068fdb62d21b96a3
13752579640 <1374134538@qq.com>
2022-05-31 10:31:34 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 #from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from lists import views
 from django.conf.urls import url
 urlpatterns = [
     #path('admin/', admin.site.urls),
     url(r'^$',views.home_page,name='home'),
-    url(r'^lists/the-only-list-in-the-world/$',views.view_list,name='view_list'),
+    url(r'^lists/new$',views.new_list,name='new_list'),
+    url(r'^lists/(\d+)/$',views.view_list,name='view_list'),
+    url(r'^lists/(\d+)/add_item$',views.add_item,name='add_item'),
 ]
